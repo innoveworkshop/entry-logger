0d6bc5e2cd991769e8d418b4234808067d238933
Nathan Campos <eeepc904@gmail.com>
2020-06-02 14:49:10 +0100

    Started creating the bascis of a document

--- Sources/eldocument.pas
+++ Sources/eldocument.pas
@@ -5,19 +5,61 @@ unit ELDocument;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, Column;
 
 type
-  {TELDocument}
+  { TELDocument }
   TELDocument = class
     private
-      test: Integer;
+      FColumns: array of TColumn;
+      function GetColumn(Index: Integer): TColumn;
 
     public
-      anothertest: Integer;
+      property Columns[Index: Integer]: TColumn read GetColumn;
+
+      constructor Create; overload;
+
+      function ToString: String; override;
   end;
 
 implementation
+
+{ Constructs an empty Entry Logger Document. }
+constructor TELDocument.Create;
+var
+  i: Integer;
+begin
+  { Construct and populate the array. }
+  SetLength(FColumns, 10);
+  for i := 0 to Length(FColumns) - 1 do
+  begin
+    FColumns[i] := TColumn.Create('Column ' + IntToStr(i));
+  end;
+end;
+
+{ Gets a column from the array. }
+function TELDocument.GetColumn(Index: Integer): TColumn;
+begin
+  GetColumn := FColumns[Index];
+end;
+
+{ Gets a string representation of this object. }
+function TELDocument.ToString: String;
+var
+  sBuffer: String;
+  nIndex: Integer;
+  colCurrent: TColumn;
+begin
+  { Go through the array of columns. }
+  sBuffer := 'Columns:' + sLineBreak;
+  for nIndex := 0 to Length(FColumns) - 1 do
+  begin
+    colCurrent := FColumns[nIndex];
+    sBuffer := sBuffer + '    ' + colCurrent.Name + sLineBreak;
+  end;
+
+  ToString := sBuffer;
+end;
 
 end.
 
--- Sources/main.pas
+++ Sources/main.pas
@@ -5,12 +5,11 @@ unit Main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, Grids, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, StdCtrls,
+  ELDocument;
 
 type
-
   { TfrmMain }
-
   TfrmMain = class(TForm)
     txtConsole: TMemo;
     mnuWindow: TMenuItem;
@@ -24,8 +23,9 @@ type
     N1: TMenuItem;
     mnuMain: TMainMenu;
     procedure FormCreate(Sender: TObject);
-  private
 
+  private
+    elDocument: TELDocument;
   public
 
   end;
@@ -43,6 +43,9 @@ implementation
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
   txtConsole.Append('Welcome to Entry Logger!');
+
+  elDocument := TELDocument.Create;
+  txtConsole.Append(elDocument.ToString);
 end;
 
 end.
