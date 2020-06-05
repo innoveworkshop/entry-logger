66acd001a97926a65db7729efafe98621fb361d1
Nathan Campos <eeepc904@gmail.com>
2020-06-05 12:40:10 +0100

    Laid out the Entry Model form

---
+++ Entry Logger/ModelForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace EntryLogger {
+	partial class ModelForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.lstModels = new System.Windows.Forms.ListBox();
+			this.btnRemove = new System.Windows.Forms.Button();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnClear = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// lstModels
+			// 
+			this.lstModels.FormattingEnabled = true;
+			this.lstModels.ItemHeight = 15;
+			this.lstModels.Location = new System.Drawing.Point(12, 12);
+			this.lstModels.Name = "lstModels";
+			this.lstModels.Size = new System.Drawing.Size(205, 139);
+			this.lstModels.TabIndex = 0;
+			// 
+			// btnRemove
+			// 
+			this.btnRemove.Location = new System.Drawing.Point(223, 41);
+			this.btnRemove.Name = "btnRemove";
+			this.btnRemove.Size = new System.Drawing.Size(75, 23);
+			this.btnRemove.TabIndex = 1;
+			this.btnRemove.Text = "Remove";
+			this.btnRemove.UseVisualStyleBackColor = true;
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Location = new System.Drawing.Point(223, 12);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(75, 23);
+			this.btnAdd.TabIndex = 1;
+			this.btnAdd.Text = "Add";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 154);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(39, 15);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Name";
+			// 
+			// btnClear
+			// 
+			this.btnClear.Location = new System.Drawing.Point(223, 128);
+			this.btnClear.Name = "btnClear";
+			this.btnClear.Size = new System.Drawing.Size(75, 23);
+			this.btnClear.TabIndex = 1;
+			this.btnClear.Text = "Clear";
+			this.btnClear.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 172);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(158, 23);
+			this.textBox1.TabIndex = 3;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(176, 172);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(122, 23);
+			this.comboBox1.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(176, 154);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(31, 15);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Type";
+			// 
+			// ModelForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(310, 207);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.btnClear);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnAdd);
+			this.Controls.Add(this.btnRemove);
+			this.Controls.Add(this.lstModels);
+			this.Name = "ModelForm";
+			this.Text = "Entry Model";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox lstModels;
+		private System.Windows.Forms.Button btnRemove;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnClear;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label2;
+	}
+}
---
+++ Entry Logger/ModelForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EntryLogger {
+	public partial class ModelForm : Form {
+		public ModelForm() {
+			InitializeComponent();
+		}
+	}
+}
